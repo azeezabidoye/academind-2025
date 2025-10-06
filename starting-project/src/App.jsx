5f64baed7af9c7fce08d2d2f579fa643256e1484
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-06 15:47:10 +0100

    Official Commit

---
+++ starting-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Post from "./components/Post";
+
+function App(props) {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Post author="Currency" body="Werey la fi n wo werey" />
+      <Post author="Wizkid" body="Ojuelegba...them know my story" />
+    </div>
+  );
+}
+
+export default App;
