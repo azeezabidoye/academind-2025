6977a1d4590a5ad818ea79f181197f681b933908
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-07 15:48:37 +0100

    Components Updated

--- starting-project/src/components/NewPost.jsx
+++ starting-project/src/components/NewPost.jsx
@@ -1,11 +1,14 @@
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  function handleOnChange(e) {
+    console.log(e.target.value);
+  }
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={handleOnChange} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
