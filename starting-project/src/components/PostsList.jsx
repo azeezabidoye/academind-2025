b5a486597795ee436bd5214c0bf5de4980f7718c
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-07 15:45:46 +0100

    Components Updated

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Currency" body="Werey la fi n wo werey" />
-      <Post author="Wizkid" body="Ojuelegba...them know my story" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Currency" body="Werey la fi n wo werey" />
+        <Post author="Wizkid" body="Ojuelegba...them know my story" />
+      </ul>
+    </>
   );
 }
 
