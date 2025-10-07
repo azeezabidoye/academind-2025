3914deffbefe2f9ed3212dd1940ab9b4418dfc3a
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-07 12:38:13 +0100

    Component Updated

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -3,7 +3,6 @@ import Post from "./components/Post";
 function App(props) {
   return (
     <div>
-      <h1>Hello World!</h1>
       <Post author="Currency" body="Werey la fi n wo werey" />
       <Post author="Wizkid" body="Ojuelegba...them know my story" />
     </div>
