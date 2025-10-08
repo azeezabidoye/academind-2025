e6369b33940eb9809552990cafa7413ddf73fc65
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-08 14:47:08 +0100

    Update

--- starting-project/src/components/Modal.jsx
+++ starting-project/src/components/Modal.jsx
@@ -1,5 +1,12 @@
 import classes from "./Modal.module.css";
 
-function Modal() {}
+function Modal({ children }) {
+  return (
+    <>
+      <div className={classes.backdrop} />
+      <dialogue className={classes.modal}>{children}</dialogue>
+    </>
+  );
+}
 
 export default Modal;
