0b68c95a8675e7b2482b326b0c30bd5cf9697742
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-09 13:11:09 +0100

    Update

--- starting-project/src/components/NewPost.jsx
+++ starting-project/src/components/NewPost.jsx
@@ -2,16 +2,25 @@ import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost(props) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(e) {
+    setEnteredBody(e.target.value);
+  }
+  function changeAuthorHandler(e) {
+    setEnteredAuthor(e.target.value);
+  }
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
 
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
+        <input type="text" id="name" required onChange={changeAuthorHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,20 +1,8 @@
-import { useState } from "react";
 import Post from "./Post";
-import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function changeBodyHandler(e) {
-    setEnteredBody(e.target.value);
-  }
-  function changeAuthorHandler(e) {
-    setEnteredAuthor(e.target.value);
-  }
-
   return (
     <>
       {isPosting && (
